6826ebd56dc2d6c5edb57885a7e0c88e4b9fbad2
Antoine Sablayrolles <sablayrolles.antoine@gmail.com>
2019-02-12 11:06:21 +0100

    ex03 et ex04

---
+++ j05/ex03/main.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asablayr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/12 09:00:14 by asablayr          #+#    #+#             */
+/*   Updated: 2019/02/12 11:00:50 by asablayr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strcpy(char *dest, char *src);
+
+int		main(void)
+{
+	char	 qwe[8] = "bonjour";
+	char	asd[8];
+	char	zxc[8];
+	char	*source1;
+	char	*source2;
+	char	*yours;
+	char	*theirs;
+	int		DIFF;	
+
+	source1 = &qwe[0];
+	source2 = &qwe[0];
+	yours = &asd[0];
+	theirs = &zxc[0];
+	yours = ft_strcpy(yours, source1);
+	theirs = strcpy(theirs, source2); 
+	DIFF = strcmp(&qwe[0], &asd[0]);
+	printf("input : %s		", qwe);
+	printf("your output : %s	", asd);
+	printf("official output : %s	", zxc);
+	printf("DIFF = %d", DIFF);
+	return (0);
+}
---
+++ j05/ex04/main.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asablayr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/12 10:04:53 by asablayr          #+#    #+#             */
+/*   Updated: 2019/02/12 10:51:25 by asablayr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+
+int		main(void)
+{
+	char qwe[] = "bonjour";
+	char asd[8];
+	char zxc[8];
+	char *source1;
+	char *source2;
+	char *yours;
+	char *their;
+	int DIFF;
+
+	source1 = &qwe[0];
+	source2 = &qwe[0];
+	yours = &asd[0];
+	their = &zxc[0];
+	yours = ft_strncpy(yours, source1, 4);
+	their = strncpy(their, source2, 4);
+	DIFF = strcmp(&zxc[0], &asd[0]);
+	printf("input : %s		", qwe);
+	printf("your output : %s	", asd);
+	printf("official output : %s	", zxc);
+	printf("DIFF = %d\n", DIFF);
+	return (0);
+}
