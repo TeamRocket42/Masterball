ac5268ca036014f4e73885190d314c057770c5fa
HEhlinger <47533197+HEhlinger@users.noreply.github.com>
2019-02-12 11:58:59 +0100

    Update main.c

--- j03/ex01/main.c
+++ j03/ex01/main.c
@@ -18,14 +18,14 @@ int		main(void)
 {
 	int nbr;
 	int *p1 = &nbr;
-	int *p2 = &p1;
-	int *p3 = &p2;
-	int *p4 = &p3;
-	int *p5 = &p4;
-	int *p6 = &p5;
-	int *p7 = &p6;
-	int *p8 = &p7;
-	int *p9 = &p8;
+	int **p2 = &p1;
+	int ***p3 = &p2;
+	int ****p4 = &p3;
+	int *****p5 = &p4;
+	int ******p6 = &p5;
+	int *******p7 = &p6;
+	int ********p8 = &p7;
+	int *********p9 = &p8;
 
 	nbr = 0;
 	ft_ultimate_ft(p9);
