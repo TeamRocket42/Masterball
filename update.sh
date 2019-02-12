abd3a9566b17d62e49c23fec11f5115ecdedb56d
Louis D'allende <louis@razorbak.fr>
2019-02-12 14:56:06 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,4 +2,5 @@ printf "[\e[94mMASTERBALL\e[0m]$> \e[93mUpdate started...\e[0m\r"
 git reset --hard HEAD
 git checkout origin/master
 git branch -D master
-git checkout -b masterprintf "[\e[94mMASTERBALL\e[0m]$> \e[92mUpdate finished.        [DONE]\e[0m\n"
+git checkout -b 
+masterprintf "[\e[94mMASTERBALL\e[0m]$> \e[92mUpdate finished.        [DONE]\e[0m\n"
