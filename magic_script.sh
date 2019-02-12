9fcb0b3203f15b524c4bd271e3ad9269e0291c87
Antoine Sablayrolles <sablayrolles.antoine@gmail.com>
2019-02-12 14:42:17 +0100

    update magic script

--- magic_script.sh
+++ magic_script.sh
@@ -2,9 +2,9 @@ echo path to the file of the day
 read pathname
 if [ -d "$pathname" ]
 then
-	echo ok for the path\n
+	echo ok for the path
 else
-	echo couldn\'t find directory\n
+	echo couldn\'t find directory
 fi
 
 echo what day are we correcting ?
@@ -38,26 +38,26 @@ then
 
 elif [ "$daynb" = "j05" ]
 then
-	cp $daynb/ex00/main.c $pathname/ex00/main.c
-	cp $daynb/ex01/main.c $pathname/ex01/main.c
-	cp $daynb/ex02/main.c $pathname/ex02/main.c
-	cp $daynb/ex03/main.c $pathname/ex03/main.c
-	cp $daynb/ex04/main.c $pathname/ex04/main.c
-	cp $daynb/ex05/main.c $pathname/ex05/main.c
-	cp $daynb/ex06/main.c $pathname/ex06/main.c
-	cp $daynb/ex07/main.c $pathname/ex07/main.c
-	cp $daynb/ex08/main.c $pathname/ex08/main.c
-	cp $daynb/ex09/main.c $pathname/ex09/main.c
-	cp $daynb/ex10/main.c $pathname/ex10/main.c
-	cp $daynb/ex11/main.c $pathname/ex11/main.c
-	cp $daynb/ex12/main.c $pathname/ex12/main.c
-	cp $daynb/ex13/main.c $pathname/ex13/main.c
-	cp $daynb/ex14/main.c $pathname/ex14/main.c
-	cp $daynb/ex15/main.c $pathname/ex15/main.c
-	cp $daynb/ex16/main.c $pathname/ex16/main.c
-	cp $daynb/ex17/main.c $pathname/ex17/main.c
-	cp $daynb/ex18/main.c $pathname/ex18/main.c
-	cp $daynb/ex19/main.c $pathname/ex19/main.c
-	cp $daynb/ex20/main.c $pathname/ex20/main.c
-	cp $daynb/ex21/main.c $pathname/ex21/main.c
+	cp $daynb/main00.c $pathname/ex00/main.c
+	cp $daynb/main01.c $pathname/ex01/main.c
+	cp $daynb/main02.c $pathname/ex02/main.c
+	cp $daynb/main03.c $pathname/ex03/main.c
+	cp $daynb/main04.c $pathname/ex04/main.c
+	cp $daynb/main05.c $pathname/ex05/main.c
+	cp $daynb/main06.c $pathname/ex06/main.c
+	cp $daynb/main07.c $pathname/ex07/main.c
+	cp $daynb/main08.c $pathname/ex08/main.c
+	cp $daynb/main09.c $pathname/ex09/main.c
+	cp $daynb/main10.c $pathname/ex10/main.c
+	cp $daynb/main11.c $pathname/ex11/main.c
+	cp $daynb/main12.c $pathname/ex12/main.c
+	cp $daynb/main13.c $pathname/ex13/main.c
+	cp $daynb/main14.c $pathname/ex14/main.c
+	cp $daynb/main15.c $pathname/ex15/main.c
+	cp $daynb/main16.c $pathname/ex16/main.c
+	cp $daynb/main17.c $pathname/ex17/main.c
+	cp $daynb/main18.c $pathname/ex18/main.c
+	cp $daynb/main19.c $pathname/ex19/main.c
+	cp $daynb/main20.c $pathname/ex20/main.c
+	cp $daynb/main21.c $pathname/ex21/main.c
 fi
