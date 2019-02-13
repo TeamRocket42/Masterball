dcafff2c21b1409b42687de7b1d815e4f7387685
Hugo <hehlinge@e3r12p11.42.fr>
2019-02-13 14:17:52 +0100

    Ajour main05.c d05

--- j05/main05.c
+++ j05/main05.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hehlinge <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/10 22:25:11 by hehlinge          #+#    #+#             */
-/*   Updated: 2019/02/12 11:55:40 by hehlinge         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 #include <string.h>
 
@@ -24,5 +12,33 @@ int main()
 	char *to_find;
 	to_find = "abc";
 	printf("ft_strstr = %s\n", ft_strstr(str, to_find));
-	printf("rr_strstr = %s\n", strstr(str2, to_find));
+	printf("rr_strstr = %s\n\n", strstr(str2, to_find));
+
+	char *str3;
+	str3 = "aaaaabcde";
+	char *str4;
+	str4 = "aaaaabcde";
+	char *to_find2;
+	to_find2 = "45";
+	printf("ft_strstr = %s\n", ft_strstr(str3, to_find2));
+	printf("rr_strstr = %s\n\n", strstr(str4, to_find2));
+
+	char *str5;
+	str5 = "aaaaabcde";
+	char *str6;
+	str6 = "aaaaabcde";
+	char *to_find3;
+	to_find3 = "";
+	printf("ft_strstr = %s\n", ft_strstr(str5, to_find3));
+	printf("rr_strstr = %s\n\n", strstr(str6, to_find3));
+
+	char *str7;
+	str7 = "";
+	char *str8;
+	str8 = "";
+	char *to_find4;
+	to_find4 = "45";
+	printf("ft_strstr = %s\n", ft_strstr(str7, to_find4));
+	printf("rr_strstr = %s\n\n", strstr(str8, to_find4));
 }
+
