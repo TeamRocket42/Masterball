5271192c3d5999dc4aeb45d745f5e404eabeed21
Loic FALKAU <lfalkau@e3r12p21.42.fr>
2019-02-22 09:26:39 +0100

    ultimate main splitwhitespaces

--- j07/ex04/main.c
+++ j07/ex04/main.c
@@ -1,31 +1,39 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: scarpent <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/18 12:46:20 by scarpent          #+#    #+#             */
-/*   Updated: 2019/02/18 14:49:16 by scarpent         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+//Ce main vous est propose par votre tres chere tean Rocket !
 
 #include <stdio.h>
 
-char	**ft_split_whitespaces(char *str);
+char    **ft_split_whitespaces(char *str);
 
-int		main(int argc, char **argv)
+int main()
 {
-	char	**tab;
+	char str0[] = "la team rocket s'envole vers d'autres ciels !";
+	char str1[] = " Miaous   vs   Norminet ?\n\n\nQui gagnera la bataille ? ";
+	char str2[] = "\tscarpent\t\thellinge\nldallende\t\t\t\tasablayr         lfalkau\t   \n  \t";
+	char str3[] = "Masterball ?\nAlright !";
 
-	if (argc == 2)
-	{
-		tab = ft_split_whitespaces(argv[1]);
-		while (*tab)
-		{
-			printf("%s\n", *tab);
-			tab++;
-		}
-	}
+	printf("expected : la.team.rocket.s'envole.vers.d'autres.ciels.!.\n");
+	printf("Your output : ");
+	for (int i = 0; i < ft_count_words(str0); i++)
+		printf("%s.", ft_split_whitespaces(str0)[i]);
+	printf("\n");
+
+	printf("expected : Miaous.vs.Norminet.?.Qui.gagnera.la.bataille.?.\n");
+	printf("Your output : ");
+	for (int i = 0; i < ft_count_words(str1); i++)
+		printf("%s.", ft_split_whitespaces(str1)[i]);
+	printf("\n");
+
+	printf("expected : scarpent.hellinge.ldallende.asablayr.lfalkau.\n");
+	printf("Your output : ");
+	for (int i = 0; i < ft_count_words(str2); i++)
+		printf("%s.", ft_split_whitespaces(str2)[i]);
+	printf("\n");
+
+	printf("expected : Materball.?.Alright.!.\n");
+	printf("Your output : ");
+	for (int i = 0; i < ft_count_words(str3); i++)
+		printf("%s.", ft_split_whitespaces(str3)[i]);
+	printf("\n");
+
 	return (0);
 }
